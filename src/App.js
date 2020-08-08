4a434583b510cf40e3b629bb28bafa6c46641c95
cchaves <cchaves@isthmusit.com>
2020-08-07 20:51:22 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Order from './components/Order/Order';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/ordena">Ordena</Link>
+            </li>
+            <li>
+              <Link to="/otrapagina">OtraPagina</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/ordena">
+          <Order></Order>
+          </Route>         
+          <Route path="/otrapagina">
+            <h1>Aqui ira otro componente</h1>
+          </Route>
+          <Route path="/">
+           <Home></Home>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/components/Order/Order.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Order extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div id="order">
+                <h1>Ordena Crepas aqui</h1>
+            </div>
+        )
+    }
+}
+
+export default Order
