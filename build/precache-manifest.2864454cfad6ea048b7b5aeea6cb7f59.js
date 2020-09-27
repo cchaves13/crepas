2e9bcf40b2e90c964b89006ba6f1c319f2b1e22f
carlosfabianchaves <cchaves@isthmusit.com>
2020-09-26 21:29:29 -0600

    agregando prueba prod

---
+++ build/precache-manifest.2864454cfad6ea048b7b5aeea6cb7f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69f7555993073e82c0563a6d1af5b25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9f656ba67f91cfa259",
+    "url": "/static/css/main.ae55ed10.chunk.css"
+  },
+  {
+    "revision": "0fa303f7a153ba4e1bb5",
+    "url": "/static/js/2.730e0ab8.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.730e0ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9f656ba67f91cfa259",
+    "url": "/static/js/main.87b6c67c.chunk.js"
+  },
+  {
+    "revision": "a8b5f3310f70800be154",
+    "url": "/static/js/runtime-main.9d1df2a4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
