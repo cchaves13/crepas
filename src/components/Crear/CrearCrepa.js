9fc26899b470c8a39ac42456ba6ce801f280236e
pao27 <jbpaola27@gmail.com>
2020-09-28 22:36:08 -0600

    Relleno version 1

--- src/components/Crear/CrearCrepa.js
+++ src/components/Crear/CrearCrepa.js
@@ -22,6 +22,31 @@ export default class Crearcrepa extends Component {
                 <div className="swiper-wrapper">
                     <div className="swiper-slide">
                         <h1>Elije el Relleno</h1>
+                        {/*<input id="checkboxid" type="checkbox" class="css-checkbox"></input>*/}
+                        {/*<label for="checkboxid" class="css-label"></label>*/}
+                        <ul>
+                            <li>
+                                <input type="checkbox" id="myCheckbox1" />
+                                <label for="myCheckbox1"><img src="/imgs/MarmeladaMora.PNG" /></label>
+                            </li>
+                            <li>
+
+                                <input type="checkbox" id="myCheckbox2" />
+                                <label for="myCheckbox2"><img src="/imgs/Nutella.PNG" /></label>
+                            </li>
+                            <li>
+
+                                <input type="checkbox" id="myCheckbox3" />
+                                <label for="myCheckbox3"><img src="/imgs/MermeladaFresa.PNG" /></label>
+                            </li>
+                            <li>
+
+                            <input type="checkbox" id="myCheckbox4" />
+                            <label for="myCheckbox4"><img src="/imgs/lecheCondensada.PNG" /></label>
+                            </li>
+                            
+                            
+                            </ul>
                     </div>
                     <div className="swiper-slide">
                         <h1>Elije la Fruta</h1>
