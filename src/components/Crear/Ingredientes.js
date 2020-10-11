358d25f874c3705441095e48adeaf6822088de86
carlosfabianchaves <cchaves@isthmusit.com>
2020-10-10 18:26:14 -0600

    mejoras diseño parte1

--- src/components/Crear/Ingredientes.js
+++ src/components/Crear/Ingredientes.js
@@ -19,11 +19,23 @@ class Ingredientes extends Component{
         this.setState({Ingrediente:this.props.Ingrediente});
     }
 
+    // render(){
+    //     return(
+    //         <div className="contenedor-Ingrediente">
+    //             <input id={"checkbox"+this.state.Ingrediente.Nombre} type="checkbox"  onClick={()=>{this.props.handleClick(this.state.Ingrediente, this.props.CantMax,  this.props.Tipo)}} checked={this.state.Ingrediente.isChecked} />
+    //             <label htmlFor={"checkbox"+this.state.Ingrediente.Nombre}><img src={"/imgs/"+ this.state.Ingrediente.Nombre +".PNG"}/></label>
+    //         </div>
+    //     )
+    // }
+
     render(){
+        var divClasses = this.state.Ingrediente.isChecked ? "contenedor-Ingrediente checked" : "contenedor-Ingrediente";
+        var pictureName = this.state.Ingrediente.Nombre.replace(/\s/g,'');
+        console.log(pictureName);
+        var backgroundImage = "/imgs/"+ pictureName +".PNG";
+
         return(
-            <div className="contenedor-Ingrediente">
-                <input id={"checkbox"+this.state.Ingrediente.Nombre} type="checkbox"  onClick={()=>{this.props.handleClick(this.state.Ingrediente, this.props.CantMax,  this.props.Tipo)}} checked={this.state.Ingrediente.isChecked} />
-                <label htmlFor={"checkbox"+this.state.Ingrediente.Nombre}><img src={"/imgs/"+ this.state.Ingrediente.Nombre +".PNG"}/></label>
+            <div className={divClasses} style={{backgroundImage: 'url(' + backgroundImage + ')'}} onClick={()=>{this.props.handleClick(this.state.Ingrediente, this.props.CantMax,  this.props.Tipo)}}>                
             </div>
         )
     }
