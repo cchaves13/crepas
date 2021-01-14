8d03d9196edb9231404606e42d5a5979c2bdd172
carlosfabianchaves <cchaves@isthmusit.com>
2021-01-13 21:15:07 -0600

    Updates

---
+++ precache-manifest.570cb92eff9d51820eb6bd7f6022f983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841ad78f38395576b05fd3a0597a2de4",
+    "url": "/crepas/index.html"
+  },
+  {
+    "revision": "f264c066633d8b915e0a",
+    "url": "/crepas/static/css/main.6e3e067d.chunk.css"
+  },
+  {
+    "revision": "02be27d1cd7c783f4c39",
+    "url": "/crepas/static/js/2.0bae3d8f.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/crepas/static/js/2.0bae3d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f264c066633d8b915e0a",
+    "url": "/crepas/static/js/main.6d188c48.chunk.js"
+  },
+  {
+    "revision": "020af352598c963182e4",
+    "url": "/crepas/static/js/runtime-main.bc16f770.js"
+  }
+]);
